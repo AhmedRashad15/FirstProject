b14e57519b30df1b2c6d3ecab5b377a1268b03a3
Smart Lap <ahmedmohamed@255106@gmail.com>
2024-08-17 11:28:39 +0300

    Adding environment setup and new test class

---
+++ src/test/java/testsPackage/BasicTests.java
@@ -0,0 +1,15 @@
+package testsPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BasicTests {
+@Test
+    public void setUp(){
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.quit();
+
+    }
+}
